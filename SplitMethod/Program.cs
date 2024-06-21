00d1bb0d9443466d328ad2aead0c6c1fda4b2ad3
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-21 23:53:17 +0200

    Add files via upload

---
+++ SplitMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SplitMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "3:0,2:0,3:0";
+            string[] wyniki = text.Split(',');
+            List<int> bramki = new List<int>();
+            foreach (string wynik in wyniki)
+            {
+                string[] podzielonyWynik = wynik.Split(":");
+                foreach (var bramka in podzielonyWynik)
+                {
+                    bramki.Add(int.Parse(bramka));
+                }
+            }
+
+            foreach (int bramka in bramki)
+            {
+                Console.WriteLine(bramka);
+            }
+        }
+    }
+}
